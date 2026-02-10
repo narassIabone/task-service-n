3caf1f1cdc829f5021e64261b08e6c71de5ce099
narassIabone <nail.galeev03@mail.ru>
2026-02-11 02:47:36 +0500

    Initial commit

---
+++ task-service-impl/src/main/java/ru/cinimex/taskservicen/TaskServiceNApplication.java
@@ -0,0 +1,13 @@
+package ru.cinimex.taskservicen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskServiceNApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskServiceNApplication.class, args);
+    }
+
+}
